a32b0d592000f9eb112fda9133da6288f9441a43
Satish-Masa <kakao2964@gmail.com>
2020-03-29 00:51:18 +0900

    update webserverの立ち上げの処理の追加

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -1,1 +1,12 @@
 package controllers
+
+import (
+	"CA-Tech-Dojo-Go/config"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Route() {
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil))
+}
