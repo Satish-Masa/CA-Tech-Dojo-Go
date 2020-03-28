b382f5bd4529324fb38dfcde80e2c42c7f76a1b9
Satish-Masa <kakao2964@gmail.com>
2020-03-29 00:52:24 +0900

    update user/creatが呼ばれたときの処理の追加

--- app/controllers/webserver.go
+++ app/controllers/webserver.go
@@ -5,8 +5,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
 )
 
+func creatHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles("app/views/creat.html")
+	t.Execute(w, nil)
+}
+
 func Route() {
+	http.HandleFunc("/user/creat", creatHandler)
 	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil))
 }
