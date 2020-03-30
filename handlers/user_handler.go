766469f3c9034905921c26525383fb5efe4edf4f
Satish-Masa <kakao2964@gmail.com>
2020-03-30 23:45:30 +0900

    update Router()を追加

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -1,1 +1,13 @@
 package handlers
+
+import (
+	"CA-Tech-Dojo-Go/config"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func Router() {
+	http.HandleFunc("/user/creat", creatHandler)
+	log.Fatalln(http.ListenAndServe(fmt.Sprintf(":%d", config.Config.Port), nil))
+}
