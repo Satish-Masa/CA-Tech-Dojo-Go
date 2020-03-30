3a4070204d39b3f47abe65836157f8c4958ebd6f
Satish-Masa <kakao2964@gmail.com>
2020-03-30 23:42:36 +0900

    update structを追加

--- models/user.go
+++ models/user.go
@@ -1,1 +1,22 @@
 package models
+
+type User struct {
+	name  string
+	token string
+}
+
+type UserCreatRequest struct {
+	name string
+}
+
+type UserCreatResponse struct {
+	token string
+}
+
+type UserGetResponce struct {
+	name string
+}
+
+type UserUpdateRequest struct {
+	name string
+}
