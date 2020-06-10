774eff972775b611b416b27dec1ccf43b8f09da8
Satish-Masa <kakao2964@gmail.com>
2020-06-10 23:46:04 +0900

    update repository user.go

--- domain/repository/user.go
+++ domain/repository/user.go
@@ -1,12 +1,13 @@
 package repository
 
 import (
-	"github.com/Satish-Masa/CA-Tech-Dojo-Go/application/user"
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/domain"
+	"github.com/Satish-Masa/CA-Tech-Dojo-Go/infrastructure"
+	"github.com/Satish-Masa/CA-Tech-Dojo-Go/interfaces"
 )
 
 type UserRepository interface {
-	SaveUser(domain.User) error
-	FindUser(domain.User) user.UserGetResponce
-	UpdateUser(name, token string) error
+	Save(*domain.User) (infrastructure.UserCreatResponse, error)
+	Find(*domain.User) infrastructure.UserGetResponce
+	Update(*interfaces.UserUpdateRequest) error
 }
