a354dfcef05225001824decdf823cf75cfc82736
Satish-Masa <kakao2964@gmail.com>
2020-06-10 23:45:17 +0900

    update main.go

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/config"
+	"github.com/Satish-Masa/CA-Tech-Dojo-Go/infrastructure"
+	"github.com/Satish-Masa/CA-Tech-Dojo-Go/interfaces"
+	"github.com/jinzhu/gorm"
 )
 
 func init() {
@@ -9,5 +15,16 @@ func init() {
 }
 
 func main() {
-	// interfaces.Start()
+	tmp := "%s:%s@%s/%s"
+	connect := fmt.Sprintf(tmp, config.Config.DbUser, config.Config.Password, config.Config.Tcp, config.Config.DbName)
+	driver := config.Config.SQLDriver
+	db, err := gorm.Open(driver, connect)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	repo := infrastructure.NewUserRepository(db)
+	rest := &interfaces.Rest{UserRepository: repo, GachaRepository: repo}
+	_ = rest.Start()
 }
