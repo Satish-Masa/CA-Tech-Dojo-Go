6dece643a82a2cbddb0e22db921fded2619846ee
Satish-Masa <kakao2964@gmail.com>
2020-06-12 23:54:23 +0900

    update app layer user.go

--- application/user/user.go
+++ application/user/user.go
@@ -1,13 +1,10 @@
 package user
 
 import (
-	"log"
-
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/domain"
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/domain/repository"
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/infrastructure"
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/interfaces"
-	"github.com/dgrijalva/jwt-go"
 )
 
 type UserApplication struct {
@@ -18,29 +15,6 @@ type UserCharacter struct {
 	UserCharacterID string `json: "userCharacterID"`
 	CharacterID     int    `json: "characterID"`
 	Name            string `json: "name"`
-}
-
-func creatToken(name string) (string, error) {
-	var err error
-	secret := "secret"
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"name": name,
-		"iss":  "__init__",
-	})
-	tokenString, err := token.SignedString([]byte(secret))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return tokenString, nil
-}
-
-func FetchToken(u *interfaces.UserCreatRequest) (token string, err error) {
-	token, err = creatToken(u.Name)
-	if err != nil {
-		return "", err
-	}
-
-	return token, nil
 }
 
 func (a UserApplication) SaveUser(u *domain.User) (interfaces.UserCreatRequest, error) {
