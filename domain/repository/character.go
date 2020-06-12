63dfb60c024c32990b7094e9998ccc0e77509fe5
Satish-Masa <kakao2964@gmail.com>
2020-06-12 23:52:48 +0900

    remove domain/repository

--- domain/repository/character.go
+++
@@ -1,7 +0,0 @@
-package repository
-
-import "github.com/Satish-Masa/CA-Tech-Dojo-Go/application/gacha"
-
-type CharacterRepository interface {
-	DoGacha(gacha.GachaDrawRequest) gacha.GachaDrawResponse
-}
