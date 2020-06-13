ed2fb4ea0d78b50b68b6b084ab3ccfd10f073479
Satish-Masa <kakao2964@gmail.com>
2020-06-13 11:52:48 +0900

    update infra layer user.go

--- infrastructure/user.go
+++ infrastructure/user.go
@@ -15,10 +15,6 @@ type UserGetResponce struct {
 	Name string `json: "name"`
 }
 
-type UserCreatResponse struct {
-	Token string `json: "token"`
-}
-
 type CharacterListResponse struct {
 	Characters []user.UserCharacter `json: "characters"`
 }
@@ -27,23 +23,23 @@ func NewUserRepository(conn *gorm.DB) domainUser.UserRepository {
 	return &userRepository{conn: conn}
 }
 
-func (i *userRepository) Save(u *domainUser.User) (*UserCreatResponse, error) {
+func (i *userRepository) Save(u *domainUser.User) error {
 	err := i.conn.Create(&u).Error
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return &UserCreatResponse{Token: u.Token}, nil
+	return nil
 }
 
-func (i *userRepository) Find(u *domainUser.User) *UserGetResponce {
+func (i *userRepository) Find(id int) *UserGetResponce {
 	resp := new(UserGetResponce)
-	i.conn.First(&resp, "name=?", u.Token)
+	i.conn.First(&resp, "name=?", id)
 	return resp
 }
 
-func (i *userRepository) Update(u *interfaces.UserUpdateRequest) error {
-	err := i.conn.Model(&u).Where("token=?", u.Token).Update("name", u.Name).Error
+func (i *userRepository) Update(u *interfaces.UserUpdateRequest, id int) error {
+	err := i.conn.Model(&domainUser.User).Where("id=?", id).Update("name", u.Name).Error
 	if err != nil {
 		return err
 	}
