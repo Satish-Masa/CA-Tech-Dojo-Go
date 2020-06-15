6f7f391d592929c13f27042ed8c1d3befa9d9453
Satish-Masa <kakao2964@gmail.com>
2020-06-15 15:16:55 +0900

    update app layer user.go

--- application/user/user.go
+++ application/user/user.go
@@ -3,8 +3,6 @@ package user
 import (
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/domain"
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/domain/repository"
-	"github.com/Satish-Masa/CA-Tech-Dojo-Go/infrastructure"
-	"github.com/Satish-Masa/CA-Tech-Dojo-Go/interfaces"
 )
 
 type UserApplication struct {
@@ -15,12 +13,12 @@ func (a UserApplication) SaveUser(u *domain.User) error {
 	return a.Repository.Save(u)
 }
 
-func (a UserApplication) FindUser(uid int) (infrastructure.UserGetResponce, error) {
+func (a UserApplication) FindUser(uid int) (string, error) {
 	return a.Repository.Find(uid)
 }
 
-func (a UserApplication) UpdateUser(u *interfaces.UserUpdateRequest, id int) error {
-	return a.Repository.Update(u, id)
+func (a UserApplication) UpdateUser(name string, id int) error {
+	return a.Repository.Update(name, id)
 }
 
 /* func (a UserApplication) GetList(u domain.User) (CharacterListResponse, error) {
