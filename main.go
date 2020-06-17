66608919d1054faec4c5a3b11eb584c2cc9ac091
Satish-Masa <kakao2964@gmail.com>
2020-06-17 15:46:17 +0900

    update main.go

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	domainUser "github.com/Satish-Masa/CA-Tech-Dojo-Go/domain/user"
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/infrastructure"
 	"github.com/Satish-Masa/CA-Tech-Dojo-Go/interfaces"
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 func init() {
@@ -18,8 +18,8 @@ func init() {
 }
 
 func main() {
-	tmp := "%s:%s@%s/%s"
-	connect := fmt.Sprintf(tmp, config.Config.DbUser, config.Config.Password, config.Config.Tcp, config.Config.DbName)
+	tmp := "%s:%s@/%s?charset=utf8&parseTime=True&loc=Local"
+	connect := fmt.Sprintf(tmp, config.Config.DbUser, config.Config.Password, config.Config.DbName)
 	driver := config.Config.SQLDriver
 	db, err := gorm.Open(driver, connect)
 	if err != nil {
